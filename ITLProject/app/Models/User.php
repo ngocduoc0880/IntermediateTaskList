37b2d4c9f93edbcde2f107648259c99183f1d736
duoc0880 <ngocduoc@vutatech.vn>
2021-03-16 10:46:33 +0700

    add views

--- ITLProject/app/Models/User.php
+++ ITLProject/app/Models/User.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Model implements Authenticatable
 {
-    use HasFactory, Notifiable;
+    use Authenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +38,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get all of the tasks for the user.
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
